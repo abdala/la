acbc84af55205e7c4a96c21e7d7582f64cb58fec
Abdala Cerqueira <abdala.cerqueira@gmail.com>
2014-02-04 09:51:13 -0300

    Removing acl creation from lib

--- src/La/Controller/Plugin/Acl.php
+++ src/La/Controller/Plugin/Acl.php
@@ -20,7 +20,7 @@ class La_Controller_Plugin_Acl extends Zend_Controller_Plugin_Abstract
             $role = $identity->role;
         }
         
-        if (!$acl->isAllowed($role, $resource, $privilege)) {
+        if ($acl && !$acl->isAllowed($role, $resource, $privilege)) {
             if ($controller == 'scaffold' && $table) {
                 $privilege = $table
                            . self::RESOURCE_SEPARATOR
@@ -45,44 +45,10 @@ class La_Controller_Plugin_Acl extends Zend_Controller_Plugin_Abstract
     
     protected function _getAcl()
     {
-        $acl = false;
-        
-        if (Zend_Registry::isRegistered('cache')) {
-            $cache = Zend_Registry::get('cache');
-            $acl   = $cache->load('acl');
+        if (Zend_Registry::isRegistered('acl')) {
+            return Zend_Registry::get('acl');
         }
         
-        if (!$acl) {
-            $acl          = new Zend_Acl();
-            $role         = new Auth_Model_DbTable_Role();
-            $resource     = new Auth_Model_DbTable_Resource();
-            $roleResource = new Auth_Model_DbTable_RoleResource();
-            
-            $roles     = $role->fetchAll("name <> 'Todos'");
-            $resources = $resource->getDistinctModules(); 
-            $relations = $roleResource->fetchAllRelations();
-            
-            $acl->addRole('Todos');
-            
-            foreach ($roles as $role) {
-                $acl->addRole($role['name'], 'Todos');
-            }
-            
-            foreach ($resources as $resource) {
-                $acl->addResource($resource['module']);              
-            }
-            
-            foreach ($relations as $relation) {
-                $acl->allow($relation['name'], $relation['module'], $relation['privilege']);
-            }
-            
-            if (Zend_Registry::isRegistered('cache')) {
-                $cache->save($acl, 'acl');
-            }
-        }
-        
-        Zend_Registry::set('acl', $acl);
-        
-        return $acl;
+        return false;
     }
 }
